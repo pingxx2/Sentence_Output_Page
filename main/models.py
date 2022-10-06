5df05656f53f9afc48fb8d74a355449efc96a6df
pingxx2 <balswl0229@naver.com>
2022-10-06 15:45:55 +0900

    [DB] Bookmark 완료 [CSS] Heart 완료

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Sentence(models.Model):
     # 문구 내용
@@ -7,6 +8,8 @@ class Sentence(models.Model):
     title = models.CharField("제목", max_length=300)
     # 작성자
     writer = models.CharField("작성자", max_length=100)
+    # 북마크
+    bookmark = models.ManyToManyField(User, related_name='bookmark_sentence')
 
     def __str__(self):
         return f'({self.id}) {self.title}'
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Sentence
-from django.utils import timezone
 import random
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     """
@@ -9,7 +10,26 @@ def index(request):
     """
     s_get = Sentence.objects.all()
     size = s_get.count()
-    
-    sentence = get_object_or_404(Sentence, id=random.randrange(1,size))
-    context = {'sentence':sentence}
+    random_num = random.randrange(1,size)
+    sentence = get_object_or_404(Sentence, id=random_num)
+    context = {'sentence':sentence, 'random_num':random_num}
     return render(request, 'main/index.html', context)
+
+
+@login_required(login_url='common:login')
+def vote_bookmark(request, sentence_id):
+    """
+    즐겨찾기 등록
+    """
+    heart = 0
+    sentence = get_object_or_404(Sentence, pk=sentence_id, id=sentence_id)
+    try:
+        check = get_object_or_404(Sentence, bookmark=request.user, pk=sentence_id, id=sentence_id)
+        sentence.bookmark.remove(request.user)
+        heart=0
+    except:
+        sentence.bookmark.add(request.user)
+        heart=1
+    context = {'sentence':sentence, 'heart':heart}
+    return render(request, 'main/index.html', context)
+    

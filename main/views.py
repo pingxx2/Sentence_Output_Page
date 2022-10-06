1a0bb75f43a5db7256e31ef95b55e90600d37f01
pingxx2 <balswl0229@naver.com>
2022-10-06 11:46:12 +0900

    [DB] Sentence 값 추가 [CSS] 메인 화면 완료

--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Sentence
 from django.utils import timezone
+import random
 
 def index(request):
     """
     문구 출력
     """
-    sentence = get_object_or_404(Sentence)
+    s_get = Sentence.objects.all()
+    size = s_get.count()
+    
+    sentence = get_object_or_404(Sentence, id=random.randrange(1,size))
     context = {'sentence':sentence}
     return render(request, 'main/index.html', context)
